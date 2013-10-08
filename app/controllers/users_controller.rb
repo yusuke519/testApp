aeb4ff50c4c4db73b0e7c59a00c1ff3dc97f60fc
Yusuke Iwasawa <sabumarin.dearisu@gmail.com>
2013-10-08 16:20:00 +0900

    change render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,14 +45,15 @@ class UsersController < ApplicationController
 
   def register_post
 	#render :text => params
-    @user = User.new(user_params)
-	logger.debug("register post")
-	logger.debug(params)
-      if @user.save
-		  render :text =>"Success"
-      else
-		  render :text =>"Faile"
-      end
+	 render :text => params[:name]
+   # @user = User.new(user_params)
+   # logger.debug("register post")
+   # logger.debug(params)
+   #   if @user.save
+   # 	  render :text =>"Success"
+   #   else
+   # 	  render :text =>"Faile"
+   #   end
   end
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
