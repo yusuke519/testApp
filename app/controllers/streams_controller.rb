a7b1108702c1bddad3f811139f814a0c87190873
Yusuke Iwasawa <sabumarin.dearisu@gmail.com>
2013-10-10 17:48:05 +0900

    test

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,5 +1,5 @@
 class StreamsController < ApplicationController
-	skip_before_filter :verify_authenticity_token ,:only=>[:create, :add_data]
+	skip_before_filter :verify_authenticity_token ,:only=>[:create, :set_data]
 
 	def index
 		@streams = Stream.all
