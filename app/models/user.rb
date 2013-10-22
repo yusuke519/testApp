d465ba8ce59e4be47ea9f79afafaed33f8b7d9c6
Yusuke Iwasawa <sabumarin.dearisu@gmail.com>
2013-10-22 21:10:34 +0900

    user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :data_points
-	validates_uniqueness_of :name
-	validates_uniqueness_of :email
- 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
+	#validates_uniqueness_of :name
+	#validates_uniqueness_of :email
+ 	#validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
 end
