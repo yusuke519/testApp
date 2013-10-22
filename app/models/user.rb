aea5413a10458705b0acca7073b0d1720d787d58
Yusuke Iwasawa <sabumarin.dearisu@gmail.com>
2013-10-22 21:02:58 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  has_secure_password
+	has_many :data_points
+	validates_uniqueness_of :name
+	validates_uniqueness_of :email
+	validates_length_of :password, in: 7..32, allow_nil: false, allow_blank: nil, message:"password must contain 7-32 characters or numbers"
 end
