0a2b207dc4ee7f2daea3b4a7771818afc459314d
Yusuke Iwasawa <sabumarin.dearisu@gmail.com>
2013-10-22 21:16:40 +0900

    add user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-	#validates_uniqueness_of :name
-	#validates_uniqueness_of :email
- 	#validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, on: :create
+	# users.password_hash in the database is a :string
+	include BCrypt
+
+	def password
+		@password ||= Password.new(password_hash)
+	end
+
+	def password=(new_password)
+		@password = Password.create(new_password)
+		self.password_hash = @password
+	end
+
 end
