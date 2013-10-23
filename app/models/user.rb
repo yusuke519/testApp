7b080f31f9af6a96cc06573402babc4344d33316
Yusuke Iwasawa <sabumarin.dearisu@gmail.com>
2013-10-23 11:48:18 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :name
   validates_uniqueness_of :email
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
+  validates_format_of :email, with: /[A-Z|a-z|1-9]/ 
   validates_length_of :password, in: 7..32
 end
