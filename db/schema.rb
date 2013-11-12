e0d21371be19f9c2f86341faf8e467acbb0a3652
yu_suke519 <sabumarin.dearisu@gmail.com>
2013-11-12 23:12:45 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,24 @@ ActiveRecord::Schema.define(version: 20131108081128) do
     t.float    "to_lng"
   end
 
+  create_table "members", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "members", ["email"], name: "index_members_on_email", unique: true
+  add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -71,6 +89,7 @@ ActiveRecord::Schema.define(version: 20131108081128) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
